29a25015fdccbc95fea8632032e04bf85fffc7f3
Mark Woodfine-Jones <mwdfnj@gmail.com>
2022-05-11 12:04:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [inputNumber, setInputNumber] = useState(1)
+  const [answer, setAnswer] = useState("")
+
+  const handleNumberInc = () => {
+    setInputNumber(inputNumber + 1)
+  }
+
+
+  useEffect(() => {
+    if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+      setAnswer("fizzbuzz")
+    } else if (inputNumber % 3 === 0) {
+      setAnswer("fizz")
+    } else if (inputNumber % 5 === 0) {
+      setAnswer("buzz")
+    } else {
+      setAnswer(inputNumber);
+    }
+  }, [inputNumber,])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button value={inputNumber} onClick={handleNumberInc}>Add Number</button>
+    <h2>Number is {inputNumber}</h2>
+    <h2>It's {answer}</h2>
+    </>
   );
 }
+
 
 export default App;
